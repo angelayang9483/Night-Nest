be76fa4c5168c91bd056b581feaea7b8e5231297
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-02-02 11:46:07 -0800

    implemented add button to update History

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,36 @@ import Explore from './pages/Explore.jsx'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 
+import {useState} from 'react';
+
+
 function App() {
+  const [savedLocations, setSavedLocations] = useState([]);
+
+  const handleAddLocation = (shelter) => {
+    console.log("Adding shelter:", shelter);
+    if (!savedLocations.some((loc) => loc.name === shelter.name)) {
+        setSavedLocations((prevLocations) => {
+            const newLocations = [...prevLocations, shelter];
+            console.log("Updated locations:", newLocations);
+            return newLocations;
+        });
+    } else {
+        console.log("Shelter already exists");
+    }
+};
+
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/explore" element={<Explore />} />
+          <Route path="/history" element={<History savedLocations={savedLocations} />} />
+          <Route path="/explore" element={<Explore handleAddLocation={handleAddLocation} />} />
         </Routes>
       </BrowserRouter>
       </>
   )
 }
-
 export default App

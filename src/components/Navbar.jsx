71a83057fca2f0d03aa1d92036d537b2d7ffbc79
melissa-guo <melissag2024@gmail.com>
2025-02-01 20:54:37 -0800

    updated css for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
+import '../App.css';
 
 function Navbar() {
     return (
-        <>
+        <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/history">History</Link></li>
                 <li><Link to="/explore">Explore</Link></li>
             </ul>
-        </>
+        </nav>
     )
 }
 
